f4e7d8f42f0c0098b810c3fb6a0f73a1772827aa
jviou <156534180+jviou@users.noreply.github.com>
2025-08-23 18:52:44 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+
+// Pages
+import Index from "./pages/Index";            // liste + recherche/filters
+import Sagas from "./pages/Sagas";            // vignettes des sagas (vue "Netflix")
+import SagaDetails from "./pages/SagaDetails"; // jeux d'une saga
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -15,8 +19,16 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
+          {/* Accueil */}
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
+          {/* Vignettes des sagas */}
+          <Route path="/sagas" element={<Sagas />} />
+
+          {/* Détails d'une saga (liste des jeux de la saga) */}
+          <Route path="/sagas/:name" element={<SagaDetails />} />
+
+          {/* 404 */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
